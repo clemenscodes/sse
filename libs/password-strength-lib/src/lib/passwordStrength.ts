a2d3267466a865a38f3ccb650284f51ed0a303ad
nicoschndr <nico.schneider-2@mni.thm.de>
2023-04-26 12:57:02 +0200

    Passwort Überprüfung

--- libs/password-strength-lib/src/lib/passwordStrength.ts
+++ libs/password-strength-lib/src/lib/passwordStrength.ts
@@ -3,7 +3,33 @@ export enum PasswordStrength {
     Strong,
 }
 
-export function passwordStrength(pw: string): PasswordStrength {
-    console.log(pw)
-    return PasswordStrength.Weak;
+export function passwordStrength(vname: string, nname: string, straße: string, hnr: string, plz: string, city: string, pw: string): PasswordStrength {
+    let upperCase = false;
+    let lowerCase = false;
+    let specialCharacter = false;
+    let input = false;
+
+    for(let i = 0; i < pw.length; i++){
+        const b = pw.charAt(i);
+        console.log(b.charCodeAt(0))
+        if(b === b.toUpperCase() && !upperCase && ((b.charCodeAt(0) > 64 && b.charCodeAt(0) < 91) || (b.charCodeAt(0) > 96 && b.charCodeAt(0) < 123))){
+            upperCase = true;
+        }
+        if(b === b.toLowerCase() && !lowerCase){
+            lowerCase = true;
+        }
+        if(((b.charCodeAt(0) >= 32 && b.charCodeAt(0) < 48) || (b.charCodeAt(0) > 57 && b.charCodeAt(0) < 65) || (b.charCodeAt(0) > 90 && b.charCodeAt(0) < 97) || (b.charCodeAt(0) > 123 && b.charCodeAt(0) < 127) || b.charCodeAt(0) > 168) && !specialCharacter){
+            specialCharacter = true;
+        }
+        if(pw.toLowerCase().includes(vname.toLowerCase()) || pw.toLowerCase().includes(nname.toLowerCase()) || pw.toLowerCase().includes(straße.toLowerCase()) || pw.includes(hnr) || pw.includes(plz) || pw.toLowerCase().includes(city.toLowerCase())){
+            input = true;
+        }
+    }
+
+    if(pw.length >= 8 && upperCase && lowerCase && specialCharacter && !input){
+        return PasswordStrength.Strong;
+    }else{
+        return PasswordStrength.Weak;
+    }
+
 }
