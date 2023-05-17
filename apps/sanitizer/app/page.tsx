62f96ddf3f9cf7d85ac86280b58f6ba9afb2e40f
Clemens Horn <clemens.horn@mni.thm.de>
2023-05-17 12:54:26 +0200

    chore: xss now possible

--- apps/sanitizer/app/page.tsx
+++ apps/sanitizer/app/page.tsx
@@ -1,9 +1,22 @@
-export default async function Index() {
+'use client';
+import { useState } from 'react';
+
+export default function Index() {
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+
+    const sanitizeInput = (input: string) => {
+        setOutput(input);
+    };
+
     return (
         <main>
-            <input type="text" />
-            <button>Enter</button>
-            <p></p>
+            <input
+                type="text"
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={() => sanitizeInput(input)}>Enter</button>
+            <p dangerouslySetInnerHTML={{ __html: output }} />
         </main>
     );
 }
