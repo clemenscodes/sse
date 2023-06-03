884f8d529efa9f54b247041d71eece31e3212eaf
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-03 14:50:51 +0200

    ci(notes): add deps step, refactor notes

--- libs/scripts/prod.sh
+++ libs/scripts/prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker compose -f apps/notes/docker/docker-compose.prod.yaml up &&
+docker compose -f apps/notes/docker/production/docker-compose.yaml up &&
 echo "Client running on http://localhost:4200" &&
 echo "Server running on http://localhost:3000/api"
