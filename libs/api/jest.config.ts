57b6a37ea833a632d17fce82747c704b860429f7
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-03 13:15:21 +0200

    test(api): fix tests

--- libs/api/jest.config.ts
+++ libs/api/jest.config.ts
@@ -9,6 +9,5 @@ export default {
             { tsconfig: '<rootDir>/tsconfig.spec.json' },
         ],
     },
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/libs/api',
+    moduleFileExtensions: ['ts', 'js', 'html']
 };
--- libs/api/src/user/user.controller.spec.ts
+++ libs/api/src/user/user.controller.spec.ts
@@ -1,20 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient, User } from '@prisma/api';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaService } from '../prisma/prisma.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
     let controller: UserController;
+    let userService: DeepMockProxy<UserService>;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UserController],
-            providers: [UserService],
-        }).compile();
+            providers: [UserService, PrismaService],
+        })
+            .overrideProvider(PrismaService)
+            .useValue(mockDeep<PrismaClient>())
+            .overrideProvider(UserService)
+            .useValue(mockDeep<UserService>())
+            .compile();
 
         controller = module.get<UserController>(UserController);
+        userService = module.get(UserService);
     });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
+    it('should return users from service', async () => {
+        const testUsers: User[] = [];
+        jest.spyOn(userService, 'findAll').mockResolvedValue(testUsers);
+        const users = await controller.findAll();
+        expect(users).toBe(testUsers);
     });
 });
