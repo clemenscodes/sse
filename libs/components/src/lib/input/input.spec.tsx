f69529482a091d5bb99beca1ceccc6291ebf4723
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-05 14:43:12 +0200

    tests(components): fix tests

--- libs/components/src/lib/input/input.spec.tsx
+++ libs/components/src/lib/input/input.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Input from './input';
+import { Input } from './input';
 
 describe('Input', () => {
     it('should render successfully', () => {
--- libs/components/src/lib/label/label.spec.tsx
+++ libs/components/src/lib/label/label.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Label from './label';
+import { Label } from './label';
 
 describe('Label', () => {
     it('should render successfully', () => {
