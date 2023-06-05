cce76a9e804d09aa96dc191c6ff559b6cc667e99
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-05 19:28:47 +0200

    fix(notes): add env for windows compat

--- libs/scripts/dev.sh
+++ libs/scripts/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker compose -f apps/notes/docker/development/docker-compose.yaml up &&
-echo "Client running on http://localhost:4200" &&
-echo "Server running on http://localhost:3000/api"
+docker compose -f apps/notes/docker/development/docker-compose.yaml --env-file apps/notes/windows.env up &&
+    echo "Client running on http://localhost:4200" &&
+    echo "Server running on http://localhost:3000/api"
--- libs/scripts/prod.sh
+++ libs/scripts/prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker compose -f apps/notes/docker/production/docker-compose.yaml up &&
-echo "Client running on http://localhost:4200" &&
-echo "Server running on http://localhost:3000/api"
+docker compose -f apps/notes/docker/production/docker-compose.yaml --env-file apps/notes/windows.envup &&
+    echo "Client running on http://localhost:4200" &&
+    echo "Server running on http://localhost:3000/api"
