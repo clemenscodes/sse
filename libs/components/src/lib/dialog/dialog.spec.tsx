c23e4e4856fff82500a9d8c599a82381a7f1d59a
till <tillkowo@gmail.com>
2023-06-08 10:46:36 +0200

    added tests for Registration and Dialog component

---
+++ libs/components/src/lib/dialog/dialog.spec.tsx
@@ -0,0 +1,39 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Dialog, DialogContent, DialogTrigger } from './dialog';
+
+describe('Dialog', () => {
+    it('should render successfully', () => {
+        const { baseElement } = render(
+            <Dialog>
+                <DialogTrigger>
+                    <button>Open Dialog</button>
+                </DialogTrigger>
+                <DialogContent>
+                    <p>Dialog content</p>
+                </DialogContent>
+            </Dialog>
+        );
+        expect(baseElement).toBeTruthy();
+    });
+
+    it('should open and close the dialog when trigger and close buttons are clicked', () => {
+        render(
+            <Dialog>
+                <DialogTrigger>
+                    <button>Open Dialog</button>
+                </DialogTrigger>
+                <DialogContent>
+                    <p>Dialog content</p>
+                </DialogContent>
+            </Dialog>
+        );
+
+        // Open the dialog
+        fireEvent.click(screen.getByText('Open Dialog'));
+        expect(screen.getByText('Dialog content')).toBeInTheDocument();
+
+        // Close the dialog
+        fireEvent.click(screen.getByTitle('Close'));
+        expect(screen.queryByText('Dialog content')).not.toBeInTheDocument();
+    });
+});
--- libs/components/src/lib/registration/registration.spec.tsx
+++ libs/components/src/lib/registration/registration.spec.tsx
@@ -1,9 +1,63 @@
-import { render } from '@testing-library/react';
-import Registration from './registration';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Register from './registration';
 
-describe('Registration', () => {
+describe('Register', () => {
     it('should render successfully', () => {
-        const { baseElement } = render(<Registration />);
+        const { baseElement } = render(<Register />);
         expect(baseElement).toBeTruthy();
+    });
+
+    it('should display validation errors when form fields are empty', async () => {
+        render(<Register />);
+
+        fireEvent.click(screen.getByText('Register'));
+
+        await waitFor(() => {
+            expect(screen.getByText('Username must be at least 2 characters.')).toBeInTheDocument();
+            expect(screen.getByText('Please provide a valid email.')).toBeInTheDocument();
+            expect(screen.getByText('Password must be at least 8 characters.')).toBeInTheDocument();
+            expect(screen.getByText('Password confirmation must be at least 8 characters.')).toBeInTheDocument();
+        });
+    });
+
+    it('should submit form when all fields are filled correctly', async () => {
+        render(<Register />);
+
+        fireEvent.input(screen.getByPlaceholderText('Username'), {
+            target: {
+                value: 'testUser',
+            },
+        });
+
+        fireEvent.input(screen.getByPlaceholderText('Email'), {
+            target: {
+                value: 'testuser@test.com',
+            },
+        });
+
+        fireEvent.input(screen.getByPlaceholderText('Password'), {
+            target: {
+                value: 'testPassword123',
+            },
+        });
+
+        fireEvent.input(screen.getByPlaceholderText('Confirm Password'), {
+            target: {
+                value: 'testPassword123',
+            },
+        });
+
+        const consoleSpy = jest.spyOn(console, 'log');
+
+        fireEvent.click(screen.getByText('Register'));
+
+        await waitFor(() => {
+            expect(consoleSpy).toHaveBeenCalledWith({
+                username: 'testUser',
+                email: 'testuser@test.com',
+                password: 'testPassword123',
+                passwordConfirm: 'testPassword123',
+            });
+        });
     });
 });
