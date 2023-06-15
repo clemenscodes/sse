396c7124b2b261b5a543f550169708b2c0408360
Clemens Horn <clemens.horn@mni.thm.de>
2023-06-15 22:07:21 +0200

    test(api): fix new provider in jwt guard

--- libs/api/src/modules/jwt/jwt.guard.spec.ts
+++ libs/api/src/modules/jwt/jwt.guard.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { mockDeep } from 'jest-mock-extended';
 import { AuthService } from '../auth/auth.service';
 import { CookieService } from '../cookie/cookie.service';
+import { RefreshTokenService } from '../refresh-token/refresh-token.service';
 import { JwtGuard } from './jwt.guard';
 import { JwtService } from './jwt.service';
 
@@ -10,10 +11,17 @@ describe('JwtGuard', () => {
     let authService: AuthService;
     let jwtService: JwtService;
     let cookieService: CookieService;
+    let refreshTokenService: RefreshTokenService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [JwtGuard, AuthService, JwtService, CookieService],
+            providers: [
+                JwtGuard,
+                AuthService,
+                JwtService,
+                CookieService,
+                RefreshTokenService,
+            ],
         })
             .overrideProvider(AuthService)
             .useValue(mockDeep<AuthService>())
@@ -21,12 +29,15 @@ describe('JwtGuard', () => {
             .useValue(mockDeep<JwtService>())
             .overrideProvider(CookieService)
             .useValue(mockDeep<CookieService>())
+            .overrideProvider(RefreshTokenService)
+            .useValue(mockDeep<RefreshTokenService>())
             .compile();
 
         guard = module.get(JwtGuard);
         authService = module.get(AuthService);
         jwtService = module.get(JwtService);
         cookieService = module.get(CookieService);
+        refreshTokenService = module.get(RefreshTokenService);
     });
 
     it('should be defined', () => {
@@ -34,5 +45,6 @@ describe('JwtGuard', () => {
         expect(authService).toBeDefined();
         expect(jwtService).toBeDefined();
         expect(cookieService).toBeDefined();
+        expect(refreshTokenService).toBeDefined();
     });
 });
