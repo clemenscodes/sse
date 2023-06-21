5eb68ff985c68ba3f35f3ab4e6e43df748bb9532
till <tillkowo@gmail.com>
2023-06-21 18:04:57 +0200

    fix: adjusted code according to review

--- libs/pages/src/lib/mynotes/mynotes.tsx
+++ libs/pages/src/lib/mynotes/mynotes.tsx
@@ -1,34 +1,49 @@
-import { NoteList } from '@components';
+import { NoteList, Redirect, toast } from '@components';
 import { cn } from '@styles';
 import { CreatedNote } from '@types';
-import { get, useNotesStore } from '@utils';
-import { toast } from 'libs/components/src/lib/toast/useToast';
+import { get, useNotesStore, useSessionStore } from '@utils';
 import { NextPage } from 'next';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export type MynotesProps = React.ComponentPropsWithoutRef<'div'>;
 
 export const Mynotes: NextPage<MynotesProps> = ({ ...props }) => {
     const setNotes = useNotesStore((state) => state.setNotes);
     const notes = useNotesStore((state) => state.notes);
+    const session = useSessionStore((state) => state.session);
+    const [hasMounted, setHasMounted] = useState(false);
 
     useEffect(() => {
-        const fetchNotes = async () => {
-            const url = `note/user`;
-            const { data, status } = await get<CreatedNote[]>(url);
-            if (status !== 200) {
-                toast({
-                    title: 'Failed loading your Notes',
-                    variant: 'destructive',
-                });
-                return;
-            }
-            if (data != null) {
-                setNotes(data);
-            }
-        };
-        fetchNotes();
-    }, [setNotes]);
+        setHasMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (session && hasMounted) {
+            const fetchNotes = async () => {
+                const url = `/note/user`;
+                const { data, status } = await get<CreatedNote[]>(url);
+                if (status !== 200) {
+                    toast({
+                        title: 'Failed loading your Notes',
+                        variant: 'destructive',
+                    });
+                    return;
+                }
+                if (data != null) {
+                    setNotes(data);
+                }
+            };
+            fetchNotes();
+        }
+    }, [session, hasMounted]);
+
+    if (!hasMounted) {
+        return null;
+    }
+
+    if (!session) {
+        return <Redirect href={'/login'} />;
+    }
 
     return (
         <div
