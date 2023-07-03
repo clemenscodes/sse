063dd28b6c36b5b325ca86dbbce5cf995308f7df
nico <nico.schneider-2@mni.thm.de>
2023-07-03 16:39:12 +0200

    error handling verification-token.service.ts

--- libs/api/src/modules/verification-token/verification-token.service.ts
+++ libs/api/src/modules/verification-token/verification-token.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException} from '@nestjs/common';
 import { User, VerificationToken } from '@prisma/api';
 import { fromDate } from '@utils';
 import { PrismaService } from '../prisma/prisma.service';
 import { SessionService } from '../session/session.service';
+import {Prisma} from "@prisma/api";
 
 @Injectable()
 export class VerificationTokenService {
@@ -15,32 +16,73 @@ export class VerificationTokenService {
         5 * 60 * 1000; // 1 hour
 
     async findByUserId(userId: User['id']) {
-        const user = await this.prismaService.verificationToken.findUnique({
-            where: { userId },
-        });
-        return user;
+        try {
+            const user = await this.prismaService.verificationToken.findUnique({
+                where: {userId},
+            });
+            if(!user){
+                throw new UnauthorizedException('Invalid credentials');
+            }
+            return user;
+        }catch (e) {
+            if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                throw  new InternalServerErrorException(
+                    'Failed to rtrieve User'
+                );
+            } else if (e instanceof Prisma.PrismaClientValidationError) {
+                throw new NotFoundException('User not found');
+            } else if (e instanceof NotFoundException) {
+                throw e;
+            } else {
+                throw new InternalServerErrorException(
+                    'Failed to retrieve user'
+                );
+            }
+        }
     }
 
     async deleteByToken(resetToken: VerificationToken) {
-        const { token } = resetToken;
-        return await this.prismaService.verificationToken.delete({
-            where: { token },
-        });
+        try {
+            const {token} = resetToken;
+            return await this.prismaService.verificationToken.delete({
+                where: {token},
+            });
+        } catch (e) {
+            throw new InternalServerErrorException(
+                'Error happened while deleting verification token'
+            );
+        }
     }
 
     async createToken(userId: User['id']) {
-        const token = this.sessionService.generateSessionToken();
-        const expires = fromDate(
-            VerificationTokenService.verificationTokenDefaultTTLms
-        );
-        await this.prismaService.verificationToken.create({
-            data: {
-                token,
-                expires,
-                user: { connect: { id: userId } },
-            },
-        });
-        return token;
+        try {
+            const token = this.sessionService.generateSessionToken();
+            const expires = fromDate(
+                VerificationTokenService.verificationTokenDefaultTTLms
+            );
+            const createdData = await this.prismaService.verificationToken.create({
+                data: {
+                    token,
+                    expires,
+                    user: {connect: {id: userId}},
+                },
+            });
+            if(!createdData){
+                throw new InternalServerErrorException('Verification Token registration failed');
+            }
+            return token;
+        }catch (e) {
+            if (
+                e instanceof Prisma.PrismaClientKnownRequestError &&
+                e.code === 'P2002'
+            ) {
+                throw new InternalServerErrorException('Verification Token registration failed');
+            } else if (e instanceof InternalServerErrorException) {
+                throw e;
+            } else {
+                throw new InternalServerErrorException('Verification Token registration failed');
+            }
+        }
     }
 
     async findByVerificationToken(token: VerificationToken['token']) {
